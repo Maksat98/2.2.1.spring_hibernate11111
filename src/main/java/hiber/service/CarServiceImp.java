fa04b4c308faa2a2d85c4cecda98a06e911e5772
Maksat Asanov <95422357+Maksat98@users.noreply.github.com>
2022-10-22 13:13:06 +0300

    Update CarServiceImp.java

--- src/main/java/hiber/service/CarServiceImp.java
+++ src/main/java/hiber/service/CarServiceImp.java
@@ -4,8 +4,6 @@ import hiber.dao.CarDao;
 import hiber.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -15,7 +13,7 @@ public class CarServiceImp implements CarService {
     @Autowired
     private CarDao carDao;
 
-    @Transactional
+   
     @Override
     public List<Car> listAllCars() {
         return carDao.listAllCars();
