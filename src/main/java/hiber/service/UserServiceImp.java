8b797e3b9f0c5ff598bfa7be8426ebd1f4e70d84
Maksat Asanov <95422357+Maksat98@users.noreply.github.com>
2022-10-22 13:15:02 +0300

    Update UserServiceImp.java

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -4,8 +4,6 @@ import hiber.dao.UserDao;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Service
