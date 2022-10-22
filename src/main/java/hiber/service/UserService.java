267fc92bbf93ffc9b727fa9e85a9147a688b94be
Maksat Asanov <95422357+Maksat98@users.noreply.github.com>
2022-10-22 13:13:44 +0300

    Update UserService.java

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,14 +1,9 @@
 package hiber.service;
-
 import hiber.model.User;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 public interface UserService {
     void addUser(User user);
     List<User> listUsers();
-
-    @Transactional
     User getUser(String model, int series);
 }
